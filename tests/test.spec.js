ae2552c514b488d160f43debb4dfb80750f62b62
Laurentiu Raducu <laurentiu.raducu@gmail.com>
2024-03-15 13:45:34 +0100

    Update test.spec.js

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -6,23 +6,21 @@ test.describe('My Test Suite', () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   
-  await authenticate(page);
+  await searchForWebsite(page);
 
   await browser.close();
 
   });
 });
 
-async function authenticate(page) {
-  await page.goto('https://bitheap.tech');
-  await page.click('#menu-item-1311'); 
-  await page.locator("[name='xoo-el-username']").fill(process.env.BITHEAP_USERNAME)
-  await page.locator("[name='xoo-el-password']").fill(process.env.PASS)
-  await page.locator('xpath=/html/body/div[2]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/form/button').click()
-  const text = await page.locator('css=#menu-item-1314 > a').textContent()
-  if(text != "Hello, Playwright") 
-    console.error("The authentication was not successful!")
-  await page.screenshot({ path: 'screenshot.png' });
+async function searchForWebsite(page) {
+  await page.goto('https://google.com');
+  await page.$x("//textarea[@role='combobox' and @spellcheck='false']").fill("bitheap");
+  await page.$x("//input[@value='Google Search']").click();
+  await page.$x("//h3[contains(text(), 'bitheap')]").click();
+  await page.click('#menu-item-1303');
+  await page.waitForTimeout(15000);
+  await page.$x("//a[contains(@href, 'adclick.g.doubleclick.net')]").click();
 }
 
 
